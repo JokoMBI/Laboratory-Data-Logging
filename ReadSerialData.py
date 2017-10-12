214313a0fa8bfe9fd71f44b375e4dd647c1d91ed
JokoMBI <32702165+JokoMBI@users.noreply.github.com>
2017-10-12 13:10:30 +0000

    basic function to check via terminal
    Open with terminal! After initialising arduino should send a short info text.
    Then the terminal asks for input "t or h?".
    Decide, whether you want to know the temperature ("t") or the humidity ("h"), type your choice and press enter.
    After giving back the value, it will ask again (press Ctrl+C to leave).

--- ReadSerialData.py
+++ ReadSerialData.py
@@ -3,7 +3,7 @@ import glob
 import serial
 import time
 
-
+#open port ...t.d.: implement autodetect-> which port has to be opened?
 port =serial.Serial(
     "/dev/ttyUSB0",
     baudrate=9600,
@@ -16,21 +16,45 @@ port =serial.Serial(
     dsrdtr=False,
     xonxoff=False)
 
-time.sleep(0.5)
-print(port.readline())
+c = ""      #Serial command variable
+msg = ""    #Serial incomming message
+value = ""  #numeric value (not really
+            #parsed until now, just the first 4 chars of the message)
 
-while True:
-        
-    while port.inWaiting() >0:
-        print(port.readline())
-        
-    c = ""
-    answer = input("t or h?")
+time.sleep(0.3) #waiting for initializing port and receiving a message containing
+                #information about the sensor
+
+if port.inWaiting() > 0:    #if serial data is available
+    print(port.readline())  #print the init message
+
+    
+#the following will be the "ask for temperature or humidity" function
+while True: 
+
+
+    answer = input("t or h?")   #what do you want to know, temperature or humidity?
+                                #later the value will be received from the master function
+    
+    port.flushInput()           #clear the input buffer to be shure, the value is actual
+
+    #asking for temperature
     if answer == "t":
         c = "t"
-        port.write(c.encode())
-
+        port.write(c.encode())  #send command
+        c = ""
+        
+    #asking for humidity
     if answer == "h":
         c = "h"
-        port.write(c.encode())
+        port.write(c.encode())  #send command
+        c = ""
 
+    time.sleep(0.05)            #buffering time
+    
+    if port.inWaiting() > 0:
+        msg = port.readline()   #read incoming message
+        
+        value = msg[0:4]        #take the first 4 chars
+        print(value)            #and print it
+        value = ""
+        msg= ""
